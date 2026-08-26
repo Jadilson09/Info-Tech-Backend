6a3670cc9e270db103508b21ea8d8a490098f8a7
Jadilson09 <jadsonsantosoliveira09@gmail.com>
2026-08-26 16:45:13 -0300

    chore: adicionando rotas de delete

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,6 @@ import CategoriaController from "./controller/CategoriaController.js";
 import MovimentacaoController from "./controller/MovimentacaoController.js";
 import ProdutoController from "./controller/ProdutoController.js";
 
-
 import { validate } from "./middlewares/validate.js";
 import { criarProdutoSchema, atualizarProdutoSchema } from "./schemas/produto.schema.js";
 
@@ -25,9 +24,10 @@ router.post("/api/Categorias", CategoriaController.criar);
 router.get("/api/Categorias/:idCategoria", CategoriaController.buscarPorId);
 
 // Produtos
-router.get("/api/Produtos", ProdutoController.listar);
-router.post("/api/Produtos", validate(criarProdutoSchema), ProdutoController.criar);
-router.put("/api/Produtos/:idProduto", validate(atualizarProdutoSchema), ProdutoController.atualizar);
-router.get("/api/Produtos/:idProduto", ProdutoController.buscarPorId);
+router.get("/api/Produtos", (req, res) => ProdutoController.listar(req, res));
+router.post("/api/Produtos", validate(criarProdutoSchema), (req, res) => ProdutoController.criar(req, res));
+router.get("/api/Produtos/:idProduto", (req, res) => ProdutoController.buscarPorId(req, res));
+router.put("/api/Produtos/:idProduto", validate(atualizarProdutoSchema), (req, res) => ProdutoController.atualizar(req, res));
+router.delete("/api/Produtos/:idProduto", (req, res) => ProdutoController.deletar(req, res));
 
 export { router };
