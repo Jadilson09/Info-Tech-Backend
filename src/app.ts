c09a70e9ee5881cfda4112d4884ac2f66a207ce0
Jadilson09 <jadsonsantosoliveira09@gmail.com>
2026-08-26 16:45:54 -0300

    chore: Importando express e cors no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import { DatabaseModel } from "./model/DatabaseModel.js";
 import { server } from "./server.js";
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors()); 
+app.use(express.json());
 
 const port: number = Number(process.env.PORT) || 3333; //Define a porta que o servidor vai executar
 
